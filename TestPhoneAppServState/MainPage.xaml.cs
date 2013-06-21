d7760b94ece05f2fbff205dc5a23bc478986072f
Alexandre Engelhardt <alexandre@zipion-live.be>
2013-06-21 10:08:04 +0200

    Demonstration complete
    The PhoneApplicationService class allows to store data (both value and
    object types) and use them as global variables available from anywhere
    in the app.

---
+++ TestPhoneAppServState/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+// contains the PhoneApplicationService class
+using Microsoft.Phone.Shell;
+
+namespace TestPhoneAppServState
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // we get the content from "Input"
+            // casting is needed as everything in the PAS.Current.State is stored as "object"
+            tbOutput.Text = PhoneApplicationService.Current.State["Input"] as string;
+        }
+
+        private void tbxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // we store the content of the textbox
+            PhoneApplicationService.Current.State["Input"] = tbxInput.Text;
+        }
+    }
+}
---
+++ TestPhoneAppServState/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestPhoneAppServState")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPhoneAppServState")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b40c8724-a406-4e49-acbe-196667190b8a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
